74ccefafa31e8a175644e10757ad39add47ac432
mahak-soni <mahaksoni.work@gmail.com>
2024-09-19 23:02:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const box = document.querySelector(".box");
+const toDoBox = document.querySelector("#to-do-box");
+
+box.addEventListener(
+    "keyup",
+    function (event) {
+        if (event.key == "Enter") {
+            const value = this.value;
+
+            addToDo(this.value)
+            this.value = ""
+            localStorage.setItem("list", JSON.stringify(value));
+        }
+    }
+
+)
+const addToDo = (box) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${box}
+    <i class="fas fa-times"></i>   
+    `;
+    listItem.addEventListener(
+        "click",
+        function () {
+            this.classList.toggle("done");
+        }
+    )
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function () {
+            listItem.remove();
+        }
+    )
+    toDoBox.appendChild(listItem);
+}
