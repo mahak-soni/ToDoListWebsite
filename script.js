573dcaa3b4788525f420199f7bd5dc696550af9a
Mahak Soni <mahaksoni.work@gmail.com>
2024-09-21 17:12:45 +0530

    Update script.js
    I stored the lists in localStorage and showing the list, even after refreshing the page with the help of setitem and getItem method of localStorage.

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ box.addEventListener(
 
             addToDo(this.value)
             this.value = ""
-            localStorage.setItem("list", JSON.stringify(value));
+            saveData()
+            // localStorage.setItem("list", JSON.stringify(value));
         }
     }
 
@@ -23,13 +24,23 @@ const addToDo = (box) => {
         "click",
         function () {
             this.classList.toggle("done");
+            saveData()
         }
     )
     listItem.querySelector("i").addEventListener(
         "click",
         function () {
             listItem.remove();
+            saveData()
         }
     )
     toDoBox.appendChild(listItem);
+    saveData()
 }
+function saveData(){
+    localStorage.setItem("data",toDoBox.innerHTML);
+}
+function showData(){
+    toDoBox.innerHTML=localStorage.getItem("data");
+}
+showData()
